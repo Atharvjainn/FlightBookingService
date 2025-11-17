8e34f6bbf9f3b78fbc1374e454f8b79ab2b0758a
Atharvjainn <jainatharv20@gmail.com>
2025-11-18 00:20:53 +0530

    some changes

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,6 +5,10 @@ const bookingcontroller = new BookingController()
 
 router.post('/bookings',bookingcontroller.create)
 router.post('/publish',bookingcontroller.sendMessageToQueue)
-
+router.get('/home',(req,res) => {
+    return res.status(200).json({
+        message : "hogya bhai"
+    })
+})
 
 module.exports = router
